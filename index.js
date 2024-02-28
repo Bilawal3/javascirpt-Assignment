7dfb66f76f229952b0769fbd869e7561758e912b
bilawal <bilawalsoomro616@gmail.com>
2024-02-29 01:18:45 +0500

    java

---
+++ index.js
@@ -0,0 +1,61 @@
+///////////////////////Assignment2/////////////////////////////
+
+alert("Error!Please enter a valid password")
+
+
+alert("Welcome to JS Land... \n Happy Coding")
+
+alert("Welcome to JS Land")
+
+alert("Hello... I can run JS through my brower's console")
+
+///////////////////////Assignment2/////////////////////////////
+
+var message = "Jhone Doe";
+var message1 = "15 year old";
+var message2 = "Certifed Mobile Application Development";
+
+alert(message)
+
+alert(message1)
+
+alert(message2)
+
+var message3 = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+
+alert(message3)
+
+var message4 = "my email address is";
+var message4_1 = "example@example.com";
+
+alert(message4 + message4_1)
+
+var message5 = "I am trying to leran from the Book";
+var message5_1 = "A smarter way to learn JavaScript";
+
+alert(message5 + message5_1)
+
+
+var message6 = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+alert(message6)
+
+///////////////////////Assignment3/////////////////////////////
+var message7 = "I am 19 year old";
+
+alert(message7)
+
+var message8 = "You have visited this 14 times";
+
+alert(message8)
+
+
+var message9 = "My birth year is 2005 \n Data type of my ddeclared variable is number";
+
+document.write(message9) 
+
+
+
+var message10 = "John Doe ordered 5 T-shirt(s) on XYZ Clothing store";
+
+document.write(message10)
